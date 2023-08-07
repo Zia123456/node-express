5e24e9428adc256542b436a58fb13aeaf531a5f3
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-08-07 11:11:55 -0700

    Create http-headers

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,8 @@
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' });
+  res.write('<h1>Home page</h1>');
+});
+
+server.listen(5000);
