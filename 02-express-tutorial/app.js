3893556bddc63900a823dd1ae163550ca0a55f3d
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-08-23 11:07:21 -0700

    Add index.html to public

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -5,9 +5,9 @@ const app = express();
 // setup static and middleware
 app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+// });
 
 app.get('*', (req, res) => {
   res.status(404).send('<h1>Page Not Found</h1>');
