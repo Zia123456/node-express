fe5fb3b7eb969b40721c137a1928c0def212c6b0
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-08-25 10:30:46 -0700

    Fetch single product

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,11 +4,20 @@ const { products } = require('./data');
 const { people } = require('./data');
 
 app.get('/', (req, res) => {
-  res.json(products);
+  res.send('<h1> Home Page</h1><a href="/api/products">products</a>');
 });
 
-app.get('/people', (req, res) => {
-  res.json(people);
+app.get('/api/products', (req, res) => {
+  const newProduct = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProduct);
+});
+
+app.get('/api/products/1', (req, res) => {
+  const singleProduct = products.find((product) => product.id === 1);
+  res.json(singleProduct);
 });
 
 app.listen(5000, () => {
