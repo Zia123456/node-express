07f07abd97ba2a6afee04e85385174dd42d0f471
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-08-25 09:41:04 -0700

    Send json data

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,22 +1,16 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const { products } = require('./data');
+const { people } = require('./data');
 
-// setup static and middleware
-app.use(express.static('./public'));
+app.get('/', (req, res) => {
+  res.json(products);
+});
 
-app.get('*', (req, res) => {
-  res.status(404).send('<h1>Page Not Found</h1>');
+app.get('/people', (req, res) => {
+  res.json(people);
 });
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000');
+  console.log('Server is running on port 5000...');
 });
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
