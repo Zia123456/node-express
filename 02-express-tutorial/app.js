2883efa3ce12e5b8a09e4e759f87ac20e6ff4270
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-09-01 10:39:30 -0700

    Create about route

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,7 +4,11 @@ const app = express();
 // req => middleware => res
 
 app.get('/', (req, res) => {
-  res.send('Home');
+  res.send('<h1>Home</h1>');
+});
+
+app.get('/about', (req, res) => {
+  res.send('<h1>About</h1>');
 });
 
 app.listen(5000, () => {
