cd318b1e5dacd843e7837d26e60da6d3208848b2
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-09-01 10:36:58 -0700

    Create Home route

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+// req => middleware => res
+
+app.get('/', (req, res) => {
+  res.send('Home');
+});
+
 app.listen(5000, () => {
   console.log('Server is running on port 5000...');
 });
