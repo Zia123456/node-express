1a432afff6f46b5738e4b6788b128619dd553a1b
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-09-06 10:34:56 -0700

    Remove authorize

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -19,7 +19,7 @@ app.get('/api/products', (req, res) => {
   res.send('<h1>Products</h1>');
 });
 
-app.get('/api/items', [logger, authorize], (req, res) => {
+app.get('/api/items', (req, res) => {
   res.send('<h1>Items</h1>');
 });
 
