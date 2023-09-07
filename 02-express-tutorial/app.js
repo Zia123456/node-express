eabb0a79bbc10d2e2b56daef9c9c98c94277cf8f
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-09-07 09:21:33 -0700

    Remove unused code

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,27 +1,6 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
-const logger = require('./logger');
-const authorize = require('./authorize');
 
-// req => middleware => res
-// app.use([logger, authorize]);
-app.use(morgan('tiny'));
-app.get('/', (req, res) => {
-  res.send('<h1>Welcome to home page</h1>');
-});
-
-app.get('/about', (req, res) => {
-  res.send('<h1>Welcome to about page</h1>');
-});
-
-app.get('/api/products', (req, res) => {
-  res.send('<h1>Products</h1>');
-});
-
-app.get('/api/items', (req, res) => {
-  res.send('<h1>Items</h1>');
-});
 
 app.listen(5000, () => {
   console.log('Server is running on port 5000...');
