88959193407d4ec4716f25a99881a238f0bef3c6
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-09-08 09:05:42 -0700

    Some chanhes

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,6 +3,9 @@ const app = express();
 
 let { people } = require('./data');
 
+// static assets
+app.use(express.static('./methods-public'));
+
 app.get('/api/people', (req, res) => {
   res.status(200).json({ success: true, data: people });
 });
