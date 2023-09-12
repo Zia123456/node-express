f334d372e8f9fd633dca36bb70b8bc71c90b1f8f
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-09-12 11:24:10 -0700

    Use PUT method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -42,6 +42,11 @@ app.post('/login', (req, res) => {
   res.status(401).send('Please Provide Credentials');
 });
 
+app.put('/api/people/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+});
+
 app.listen(5000, () => {
   console.log('Server is running on port 5000...');
 });
