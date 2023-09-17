67b947f8ef1b824ffc6ed503de868c4c34295907
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-09-17 08:36:54 -0700

    Use filter method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -69,6 +69,10 @@ app.delete('/api/people/:id', (req, req) => {
       .status(404)
       .json({ success: false, msg: `no person with id ${id}` });
   }
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id),
+  );
+  return res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(5000, () => {
